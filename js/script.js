87a60bbe4e7a77f27f65575a1b355d0ce3d27df8
Liryck <liryckbragin@gmail.com>
2022-11-07 15:51:16 +0200

    add task for cycles

--- js/script.js
+++ js/script.js
@@ -17,3 +17,143 @@ personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
+
+// Задачи:
+// 1. При помощи цикла выведите числа от 5 до 10 в консоль. 5 и 10 включительно. Цикл можно использовать любой
+// 2. При помощи цикла for вывести числа от 20 до 10 в консоль. В обратном порядке (20, 19, 18...). Когда цикл дойдет до числа 13 - остановить весь цикл
+// 3.При помощи цикла for выведите чётные числа от 2 до 10 включительно
+// 4.Перепишите цикл  for на вариант с while. Результат должен остаться точно таким же. Не создайте бесконечный цикл! Иначе браузер может зависнуть.
+//  Цикл, который нужно переписать:
+//  for (let i = 2; i <= 16; i++) {
+//      if (i % 2 === 0) {
+//          continue;
+//      } else {
+//          console.log(i);
+//      }
+//  }
+// 5. Заполните массив цифрами от 5 до 10 включительно. Помните, что элементы массива можно сформировать так же, как и обращаться к ним: arr[0]
+
+
+function firstTask() {
+    let num = 5;
+    while (num<11){
+        console.log(num);
+        num++;
+    }
+    
+}
+
+function secondTask() {
+    for (let i=20; i>10; i--) {
+        if (i===13) {
+            break;
+        }
+        console.log(i);
+    }
+    
+}
+
+function thirdTask() {
+    for (let i=2; i<11; i++) {
+        if ( i % 2 === 0){
+            console.log(i);
+        }
+    }
+}
+
+
+// for (let i = 2; i <= 16; i++) {
+//     if (i % 2 === 0) {
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+function fourthTask() {
+    let i = 2;
+    while (i <= 16) {
+        if (i % 2 === 0) {
+            i++;
+            continue;
+        } else {
+            console.log(i);
+        }
+        i++;
+    }
+    
+}
+
+function fifthTask() {
+    const arrayOfNumbers = [];
+    for (let i = 5; i < 11; i++) {
+        arrayOfNumbers[i - 5] = i;
+    }
+
+    console.log(arrayOfNumbers);
+    
+    // Не трогаем
+    return arrayOfNumbers;
+}
+
+// Задачи:
+
+// 1. Заполните новый массив (result) числами из старого (arr). Количество элементов в массиве можно получить как arr.length, а к элементам обращаемся все так же: arr[0], arr[1] и тд.
+// Должен получиться точно такой же массив
+
+// 2. Измените данный массив так, чтобы все числа были увеличены в 2 раза, а если попадается строка строка - то к ней было добавлено " - done".
+// Для определения типа данных используйте typeof();
+// Должно получиться: [ 10, 20, 'Shopping - done', 40, 'Homework - done' ]
+
+// 3. Разверните массив data наоборот при помощи цикла и запишите данные в массив result.
+// Должно получиться: [ 'Homework', 20, 'Shopping', 10, 5 ]
+
+// Место для первой задачи
+function firstTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const arr = [3, 5, 8, 16, 20, 23, 50];
+    const result = [];
+
+    // Пишем решение вот тут
+    for (let i = 0; i < arr.length; i++) {
+        result[i] = arr[i];
+    }
+
+    console.log(result);
+    
+    // Не трогаем
+    return result;
+}
+
+// Место для второй задачи
+function secondTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+    for (let i = 0; i < data.length; i++) {
+        if (typeof(data[i]) === 'number') {
+            data[i] = data[i] * 2;
+        }
+        if (typeof(data[i]) === 'string') {
+            data[i] = `${data[i]} - done`;
+        }
+    }   
+    console.log(data);
+    // Не трогаем
+    return data;
+}
+
+// Место для третьей задачи
+function thirdTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+    const result = [];
+
+    // Пишем решение вот тут
+    for (let i = 1; i <= data.length; i++) {
+        result[i - 1] = data[data.length - i];
+    }
+    
+    console.log(result);
+    // Не трогаем
+    return result;
+}
