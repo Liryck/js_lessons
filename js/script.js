c88806974403201d78289448ff83928d9beae250
Liryck <liryckbragin@gmail.com>
2023-02-17 16:08:34 +0200

    add task

--- js/script.js
+++ js/script.js
@@ -581,44 +581,69 @@ let personalMovieDB = {
 
 // availableCurr([...baseCurrencies, ...additionalCurrencies], 'CNY');
 
-const shoppingMallData = {
-    shops: [
-        {
-            width: 10,
-            length: 5
-        },
-        {
-            width: 15,
-            length: 7
-        },
-        {
-            width: 20,
-            length: 5
-        },
-        {
-            width: 8,
-            length: 10
+// const shoppingMallData = {
+//     shops: [
+//         {
+//             width: 10,
+//             length: 5
+//         },
+//         {
+//             width: 15,
+//             length: 7
+//         },
+//         {
+//             width: 20,
+//             length: 5
+//         },
+//         {
+//             width: 8,
+//             length: 10
+//         }
+//     ],
+//     height: 5,
+//     moneyPer1m3: 30,
+//     budget: 50000
+// };
+
+// function isBudgetEnough(data) {
+//     let square = 0;
+//     let volume = 0;
+
+//     data.shops.forEach (shop => {
+//         square += shop.width * shop.length;
+//     });
+
+//     volume = square * data.height;
+//     if (data.budget - (volume * data.moneyPer1m3) >= 0) {
+//         return 'Бюджета достаточно';
+//     } else {
+//         return 'Бюджета недостаточно';
+//     }
+// }
+
+// isBudgetEnough(shoppingMallData);
+
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+
+function sortStudentsByGroups(arr) {
+    arr.sort();
+
+    const a = [], b = [], c = [], rest = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i < 3) {
+            a.push(arr[i]);
+        } else if (i < 6) {
+            b.push(arr[i]);
+        } else if (i < 9) {
+            c.push(arr[i]);
+        } else {
+            rest.push(arr[i]);
         }
-    ],
-    height: 5,
-    moneyPer1m3: 30,
-    budget: 50000
-};
-
-function isBudgetEnough(data) {
-    let square = 0;
-    let volume = 0;
-
-    data.shops.forEach (shop => {
-        square += shop.width * shop.length;
-    });
-
-    volume = square * data.height;
-    if (data.budget - (volume * data.moneyPer1m3) >= 0) {
-        return 'Бюджета достаточно';
-    } else {
-        return 'Бюджета недостаточно';
     }
+
+    return [a,b,c, `Оставшиеся студенты: ${rest.length === 0 ? '-' : rest.join(', ')}`];
+
 }
 
-isBudgetEnough(shoppingMallData);
+sortStudentsByGroups(students);
